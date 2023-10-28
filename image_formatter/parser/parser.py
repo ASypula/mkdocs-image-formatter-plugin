768846b2a25eeba4768780b5b817591bf98320e3
ASypula <aleksandra.sypula@gmail.com>
2023-10-28 20:59:07 +0200

    feat: [parser] Added draft parse function with prints for behaviour tracking

--- image_formatter/parser/parser.py
+++ image_formatter/parser/parser.py
@@ -1,5 +1,5 @@
 from image_formatter.lexer.lexer import Lexer
-from image_formatter.lexer.token import Token, TokenType
+from image_formatter.lexer.token import TokenType
 
 class Parser:
     """
@@ -17,16 +17,16 @@ class Parser:
 
     def consume_if_token(self, token_type: TokenType) -> bool:
         """
-        Get next token from lexer If the token types are the same.
+        Get next token from lexer if the token types are the same.
 
         Args:
             token_type: type of the token to be compared
 
         Returns:
-            True: If the types are matching
-            False: If the token types are different
+            True: if the types are matching
+            False: if the token types are different
         """
-        if self.curr_token and self.curr_token.type != token_type:
+        if not self.curr_token or self.curr_token.type != token_type:
             return False
         self.curr_token = self.lexer.get_token()
         return True
@@ -34,11 +34,12 @@ class Parser:
     def parse_image_link_url(self):
         #TODO: return None or 0?
         """
-        Verify If image url can be created according to the: 
+        Verify if image url can be created according to the: 
         image_link = image_size_tag, image_url
         """
         if self.consume_if_token(TokenType.T_IMAGE_URL):
-            return "Image link found! :)"
+            print("Image link found! :)")
+            return True
         return None
 
     def parse_image_link_tag(self):
@@ -55,6 +56,15 @@ class Parser:
         TODO
         """
         while self.lexer.running:
-            x = self.parse_image_link_tag()
-            print(x)
-
+            if self.curr_token:
+                print(self.curr_token.string)
+            else:
+                print("None")
+            is_found = self.parse_image_link_tag()
+            if not is_found:
+                if not self.curr_token: self.lexer.next_char()
+                print("Not found")
+            else:
+                print("Found!")
+            self.curr_token = self.lexer.get_token()
+            print(f"{'-'*10}")
