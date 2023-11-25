4a2527c8dad515b6fb9ec7ffaadcd7362a05a910
mwasilew <01158977@pw.edu.pl>
2023-11-25 18:32:38 +0100

    feat: [token-to-string-converter] Added initial implementation

--- image_formatter/token_to_string_converter/token_to_string_converter.py
+++ image_formatter/token_to_string_converter/token_to_string_converter.py
@@ -1,2 +1,23 @@
+from image_formatter.lexer.token_sequence import TokenSequence
+from image_formatter.lexer.token import TokenType
+
+
 class TokenToStringConverter:
-    pass
+    def __init__(self, token_sequence: TokenSequence):
+        self.token_sequence = token_sequence
+
+    def to_text(self) -> str:
+        text = ""
+        for token in self.get_all_tokens():
+            if token.type == TokenType.T_IMAGE_SIZE_TAG:
+                # @TODO Refactor this
+                text += f"@{token.string}"
+            else:
+                text += token.string
+        return text
+
+    def get_all_tokens(self) -> list:
+        tokens = []
+        for t in self.token_sequence.get_token():
+            tokens.append(t)
+        return tokens
--- tests/token_to_string_converter/test_unit_token_to_string_converter.py
+++ tests/token_to_string_converter/test_unit_token_to_string_converter.py
@@ -1,1 +1,66 @@
+import io
+
+from image_formatter.lexer.lexer import Lexer
+from image_formatter.image_properties_tag_replacer.image_properties_tag_replacer import ImagePropertiesTagReplacer
 from image_formatter.token_to_string_converter.token_to_string_converter import TokenToStringConverter
+
+# @ TODO inline global var
+image_tags_properties = {
+    "small": {"height": "100px", "width": "100px"},
+    "small2": {"height": "110px", "width": "110px"},
+}
+
+
+def test_file1_literals():
+    filename = "./resources/test_files/test1.txt"
+    with open(filename, "r") as fp:
+        expected = fp.read()
+    with open(filename, "r") as fp:
+        lexer = Lexer(fp)  # noqa
+        image_tag_replacer = ImagePropertiesTagReplacer(lexer, image_tags_properties)
+        converter = TokenToStringConverter(image_tag_replacer)
+        result = converter.to_text()
+    assert expected == result
+
+
+def test_file2_mock():
+    text = """
+    1hello1 &&@small2
+@small2(some/url.com)+word
+
+    """
+    expected = """
+    1hello1 &&@small2
+(some/url.com){: style="height:110px;width:110px"}+word
+
+    """
+    fp = io.StringIO(text)
+    lexer = Lexer(fp)  # noqa
+    image_tag_replacer = ImagePropertiesTagReplacer(lexer, image_tags_properties)
+    converter = TokenToStringConverter(image_tag_replacer)
+    result = converter.to_text()
+    assert expected == result
+
+
+def test_file3_classic_macos_newline():
+    filename = "./resources/test_files/test3_classic_macos_newline.txt"
+    with open(filename, "r") as fp:
+        expected = fp.read()
+    with open(filename, "r") as fp:
+        lexer = Lexer(fp)  # noqa
+        image_tag_replacer = ImagePropertiesTagReplacer(lexer, image_tags_properties)
+        converter = TokenToStringConverter(image_tag_replacer)
+        result = converter.to_text()
+    assert expected == result
+
+
+def test_file4_unix_and_macos_newline():
+    filename = "./resources/test_files/test4_unix_and_macos_newline.txt"
+    with open(filename, "r") as fp:
+        expected = fp.read()
+    with open(filename, "r") as fp:
+        lexer = Lexer(fp)  # noqa
+        image_tag_replacer = ImagePropertiesTagReplacer(lexer, image_tags_properties)
+        converter = TokenToStringConverter(image_tag_replacer)
+        result = converter.to_text()
+    assert expected == result
