72491e0fc144ec1930506096d480166b99e55187
julia <julia.jodczyk@gmail.com>
2023-11-26 18:04:26 +0100

    feat: [tests]: Added comment to hypothesis tests

--- tests/lexer/test_hypothesis_lexer.py
+++ tests/lexer/test_hypothesis_lexer.py
@@ -2,11 +2,17 @@ from image_formatter.lexer.lexer import Lexer
 from image_formatter.lexer.token import TokenType
 from image_formatter.lexer.position import Position
 from tests.test_helpers import get_all_tokens
-import sys
 import io
-import pytest
 from hypothesis import strategies as st
 from hypothesis import given
+
+"""
+Lexer has some configurations that depend on user input (tag, special_signs, newline_characters and
+additional_path_signs). Most of them have strict list of available symbols. Special_signs doesn't
+have that. The following tests are to check if such freedom doesn't break lexer's logic.
+They are similar to unit tests in test_unit_lexer.py, but we decided to keep both, because one
+might want to execute unit tests without hypothesis tests as they are a bit more time consuming.
+"""
 
 
 def special_sign():
@@ -19,7 +25,9 @@ def special_sign_tuples():
 
 
 @given(special_sign_tuples())
-def test_given_text_when_tags_not_separated_by_spaces_then_tokens_returned(special_signs):
+def test_given_text_when_tags_not_separated_by_spaces_then_tokens_returned(
+    special_signs,
+):
     text = f"@tag1(url1.png)@one{special_signs[0]}more{special_signs[1]}tag&and{special_signs[2]}word"
     fp = io.StringIO(text)
     lexer = Lexer(fp, special_signs=special_signs)
@@ -41,7 +49,9 @@ def test_given_text_when_tags_not_separated_by_spaces_then_tokens_returned(speci
 
 
 @given(special_sign_tuples())
-def test_given_complex_text_with_special_chars_then_sequence_of_tokens_is_returned(special_signs):
+def test_given_complex_text_with_special_chars_then_sequence_of_tokens_is_returned(
+    special_signs,
+):
     text = f"word1& word2 && @tag1{special_signs[0]}tag \n\n @tag2(start{special_signs[1]}of/url.png)"
     expected_types = [
         TokenType.T_LITERAL,
