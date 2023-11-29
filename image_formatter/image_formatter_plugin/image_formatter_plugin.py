f3b6863c9db46625f2a917c5c35cf36e84a590e2
julia <julia.jodczyk@gmail.com>
2023-11-29 21:57:50 +0100

    feat: [plugin]: Fixed config passing to plugin

--- image_formatter/image_formatter_plugin/image_formatter_plugin.py
+++ image_formatter/image_formatter_plugin/image_formatter_plugin.py
@@ -11,6 +11,8 @@ import mkdocs.plugins
 import cssutils
 import logging
 from typing import Tuple
+from image_formatter.lexer.lexer import Lexer
+from image_formatter.image_properties_tag_replacer.image_properties_tag_replacer import ImagePropertiesTagReplacer
 
 WIDTH = "width"
 HEIGHT = "height"
@@ -44,7 +46,7 @@ class ImageFormatterPlugin(mkdocs.plugins.BasePlugin[ImageFormatterConfig]):
         """
         Verifies if tags are defined correctly. Each tag should specify width and height in valid CSS form.
         """
-        size_tags = config["image_formatter"]
+        size_tags = self.config["image_size"]
         for tag, options in size_tags.items():
             if WIDTH not in options or HEIGHT not in options:
                 raise mkdocs.config.base.ValidationError(f"width or height is missing from {tag} tag configuration")
@@ -57,5 +59,11 @@ class ImageFormatterPlugin(mkdocs.plugins.BasePlugin[ImageFormatterConfig]):
 
     def on_page_read_source(self, page: Page, config: MkDocsConfig) -> str or None:
         # todo: using lexer, image_properties_tag_replacer and interpreter read user's docs and apply sizes specified in tags
-
-        pass
+        src_path = page.file.abs_src_path
+        # with open(src_path) as fp:
+        #     with open("test.txt", "w+") as t:
+        #         lexer = Lexer(fp)
+        #         lexer.next_char()
+        #         tags_replacer = ImagePropertiesTagReplacer(lexer, self.config["image_size"])
+        #         for link in tags_replacer.get_token():
+        #             t.write(link.string)
