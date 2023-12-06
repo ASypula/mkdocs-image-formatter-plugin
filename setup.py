2eb0e17ccfbfb23779c073f44e13b493aee4b1f6
mwasilew <01158977@pw.edu.pl>
2023-12-06 20:02:56 +0100

    fix: [general]: Update project version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     description="MkDocs plugin for managing image sizes",
     packages=find_packages(),
     install_requires=["mkdocs>=1.0"],
+    version="1.0.0",
     entry_points={
         "mkdocs.plugins": [
             "image-formatter = image_formatter.image_formatter_plugin.image_formatter_plugin:ImageFormatterPlugin",
